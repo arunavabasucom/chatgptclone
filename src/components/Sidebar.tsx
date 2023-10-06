2404181ddd07968ccd33d3b17e9e834d84819152
Arunava Basu <arunavabasudev@gmail.com>
2023-10-06 20:35:00 +0530

    feat: adding profile pic component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,18 +1,30 @@
+"use client"
+import { useSession } from "next-auth/react";
 import NewChat from "./NewChat";
 
 export default function Sidebar() {
+  const { data: session } = useSession();
+
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1 ">
         <div>
           {/*NewChat*/}
-          <NewChat/>
+          <NewChat />
 
           <div>{/*Model Selection*/}</div>
 
           {/*Map through the chatRows */}
         </div>
       </div>
+
+      {session && (
+        <img
+          src={session.user?.image!}
+          alt="profile image "
+          className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+        />
+      )}
     </div>
   );
 }

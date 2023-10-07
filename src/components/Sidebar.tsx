7606d08f2abc37e7fdf38b5e5afc903ffab7a1b6
Arunava Basu <arunavabasudev@gmail.com>
2023-10-07 17:28:12 +0530

    feat: adding new chat functionallity

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,28 +1,39 @@
 "use client";
 import { useSession, signOut } from "next-auth/react";
 import NewChat from "./NewChat";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { collection } from "firebase/firestore";
+import { db } from "../../firebase";
+import ChatRow from "./ChatRow";
 
 export default function Sidebar() {
   const { data: session } = useSession();
 
+  const [chats, loading, error] = useCollection(
+    session && collection(db, "users", session?.user?.email!, "chats")
+  );
+
   return (
     <div className="p-2 flex flex-col h-screen">
-      <div className="flex-1 ">
+      <div className="flex-1">
         <div>
-          {/*NewChat*/}
+          {/* NewChat */}
           <NewChat />
 
-          <div>{/*Model Selection*/}</div>
+          <div>Model Selection</div>
 
-          {/*Map through the chatRows */}
+          {/* Map through the chatRows */}
+          {chats?.docs.map((chat) => (
+            <ChatRow key={chat.id} id={chat.id} />
+          ))}
         </div>
       </div>
 
       {session && (
         <img
           onClick={() => signOut()}
-          src={session.user?.image!}
-          alt="profile image "
+          src={session.user?.image}
+          alt="profile image"
           className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
         />
       )}
